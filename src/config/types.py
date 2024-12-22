103f7770cdcee49db34d8653a20e1738963e789f
elsell <35787503+elsell@users.noreply.github.com>
2024-12-22 14:14:55 -0500

    refactor: Enable overriding config values from environment.

--- src/config/types.py
+++ src/config/types.py
@@ -1,4 +1,10 @@
 from pydantic import BaseModel
+from pydantic_settings import (
+    BaseSettings,
+    SettingsConfigDict,
+    PydanticBaseSettingsSource,
+)
+from typing import Type
 
 
 class AmazonAccount(BaseModel):
@@ -6,5 +12,19 @@ class AmazonAccount(BaseModel):
     password: str
 
 
-class Config(BaseModel):
+class Config(BaseSettings):
     amazon_accounts: list[AmazonAccount]
+
+    model_config = SettingsConfigDict(env_prefix="mmac_", case_sensitive=False)
+
+    @classmethod
+    def settings_customise_sources(
+        cls,
+        settings_cls: Type[BaseSettings],
+        init_settings: PydanticBaseSettingsSource,
+        env_settings: PydanticBaseSettingsSource,
+        dotenv_settings: PydanticBaseSettingsSource,
+        file_secret_settings: PydanticBaseSettingsSource,
+    ) -> tuple[PydanticBaseSettingsSource, ...]:
+        # Env settings take precedence over init settings
+        return env_settings, init_settings, file_secret_settings
